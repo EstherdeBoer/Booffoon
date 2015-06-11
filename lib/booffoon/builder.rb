3bdbe820622ec511ad273e0883ea85b1a406c4ec
Wojtek Kruszewski <wojtek@oxos.pl>
2015-06-11 14:53:23 +0200

    Paste some half-assed cowboy-coded lib

--- lib/booffoon/builder.rb
+++ lib/booffoon/builder.rb
@@ -1,4 +1,60 @@
 module Booffoon
 class Builder < ActionView::Helpers::FormBuilder
+  delegate :content_tag, :concat, to: :@template
+
+  AUTO = :auto
+
+  def wrapper(field_name, hint: AUTO, label: nil, label_class: "control-label", &block)
+    content_tag(:div, class: wrapper_classes(field_name).join(" ")) do
+      concat self.label(field_name, label, class: label_class)
+      concat @template.capture(&block)
+      concat errors(field_name)
+      concat self.hint(field_name, hint)
+    end
+  end
+
+  def errors(field_name)
+    if (messages = object.errors.messages[field_name]).present?
+      content_tag(:span, " " + messages.join(" "), class: "error help-block")
+    end
+  end
+
+  def hint(field_name, hint)
+    model_key = object.class.model_name.singular
+    if hint == AUTO
+      hint = @template.t("hints.#{model_key}.#{field_name}", default: "", raise: false)
+    end
+    if hint.present?
+      content_tag(:p, " " + hint, class: "help-block")
+    end
+  end
+
+  def wrapper_classes(field_name)
+    wrapper_classes = ["form-group"]
+    wrapper_classes << "has-error" if has_error?(field_name)
+    wrapper_classes
+  end
+
+  def has_error?(field_name)
+    object.errors.messages[field_name.to_sym].present?
+  end
+
+  def options_select(method, options_collection, options: {}, html_options: {})
+    collection_select(method, options_collection, :value, :label, options, html_options.reverse_merge(class: "form-control"))
+  end
+
+  %w[text_field text_area phone_field number_field url_field password_field email_field date_field].each do |method_name|
+    define_method(method_name) do |method, options = {}|
+      super(method, options.reverse_merge(class: "form-control"))
+    end
+  end
+
+  def select(method, choices = nil, options = {}, html_options = {}, &block)
+    super(method, choices, options, html_options.reverse_merge(class: "form-control"), &block)
+  end
+
+  def collection_select(method, collection, value_method, text_method, options: {}, html_options: {})
+    super(method, collection, value_method, text_method, options, html_options.reverse_merge(class: "form-control"))
+  end
 end
 end
--- test/form_builder_test.rb
+++ test/form_builder_test.rb
@@ -1,11 +1,27 @@
 require 'test_helper'
 
 class FormBuilderTest < ActionView::TestCase
-  test "builds form" do
-    article = Article.new(title: "World Peace")
-    concat (form_for(article, builder: Booffoon::Builder) do |form|
-      concat form.text_field(:title)
+  setup do
+    @article = Article.new(title: "World Peace")
+  end
+
+  test "adds form-control class to input" do
+    concat (form_for(@article, builder: Booffoon::Builder) do |form|
+      form.text_field(:title)
     end)
-    assert_select "form"
+
+    assert_select("input.form-control")
+  end
+
+  test "adds form-group and form-control classes" do
+    concat (form_for(@article, builder: Booffoon::Builder) do |form|
+      concat (form.wrapper(:title) do
+        concat form.text_field(:title)
+      end)
+    end)
+
+    assert_select("div.form-group") do
+      assert_select("input")
+    end
   end
 end
